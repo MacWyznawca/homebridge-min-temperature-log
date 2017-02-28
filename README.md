007fb87fbf7f1d93df82975d8c8a6f1bbe5936ed
Jaromir Kopp <qart@me.com>
2017-02-28 16:24:46 +0100

    v. 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # homebridge-min-temperature-log
+
 HomeBridge plugin to show daily minimal temperature.. It requires: [**[homebridge-mqtt-temperature-log-tasmota]**](https://www.npmjs.com/package/homebridge-mqtt-temperature-log-tasmota).
+
+Like this? Please buy me a beer (or coffee) ;-) <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=CK56Q7SFHEHSW"><img src="http://macwyznawca.pl/donate-paypal2.png" alt="Donate a coder" data-canonical-src="http://macwyznawca.pl/donate-paypal.svg" style="max-width:100%;"></a>
+
+[MacWyznawca.pl](http://macwyznawca.pl) Jaromir Kopp
+
+Installation
+--------------------
+    sudo npm install -g homebridge-min-temperature-log
+
+Sample HomeBridge Configuration
+--------------------
+
+{
+	
+    "bridge": {
+        "name": "Homebridge",
+        "username": "CC:22:3D:E3:CE:30",
+        "port": 51826,
+        "pin": "031-45-154"
+    },
+    
+    "description": "This is an example configuration file. You can use this as a template for creating your own configuration file.",
+	
+    "platforms": [],
+	
+	"accessories": [
+		{
+			"accessory": "max-temperature-log",
+			
+			"name": "Max temperature",
+			
+			"topic": "tele/sonoffrf200/SENSOR",
+			
+			"patchToRead":"/root/.homebridge/",
+			
+			"timeOffset": "-60",
+			
+			"freq": "10"
+		}
+	]
+}
+
+# Description of the configuration file.
+
+**"topic"** - must by the same like in **homebridge-mqtt-temperature-log-tasmota** config!
+
+**"patchToRead":"/root/.homebridge/"** - path to save text files with temperature data. Must by the same like in **homebridge-mqtt-temperature-log-tasmota** config!
+
+**timeOffset": "-60"** - timezone offset (minutes) if You chave problems witch timezone (like me).
+
+**"freq": "10"** - How often has a plugin to publish information (minutes).
---
+++ index.js
@@ -0,0 +1,193 @@
+// Minimum daily temperature Accessory plugin for HomeBridge for read min. temp from [homebridge-mqtt-temperature-log-tasmota]
+//
+// Remember to add accessory to config.json. Example:
+/* 	"accessories": [
+	{
+		"accessory": "min-temperature-log",
+
+		"name": "NAME OF THIS ACCESSORY",
+
+		"topic": "tele/sonoff/SENSOR",
+
+		"patchToRead":"/root/.homebridge/",
+
+		"timeOffset": "-60",
+
+		"freq": "10",
+
+		"manufacturer": "ITEAD",
+		"model": "Sonoff TH",
+		"serialNumberMAC": "MAC OR SERIAL NUMBER"
+
+	}]
+*/
+// When you attempt to add a device, it will ask for a "PIN code".
+// The default code for all HomeBridge accessories is 031-45-154.
+
+var inherits = require('util').inherits;
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+	homebridge.registerAccessory("homebridge-min-temperature-log", "min-temperature-log", MinTemperatureLogAccessory);
+}
+
+function convertDateUTCDtoLocalStr(date, timeOffset) {
+	date = new Date(date);
+	var localOffset;
+	if(timeOffset != 0){
+		localOffset = timeOffset * 60000;
+	}
+	else {
+		localOffset = date.getTimezoneOffset() * 60000;
+	}
+	var localTime = date.getTime();
+	date = localTime - localOffset;
+	date = (new Date(date)).toISOString().replace(/T/, ' ').replace(/\..+/, '');
+	return date;
+}
+
+function MinTemperatureLogAccessory(log, config) {
+	this.fs = require("graceful-fs");
+
+	this.log = log;
+	this.name = config["name"] || "Sonoff";
+	this.manufacturer = config["manufacturer"] || "MacWyznawca";
+	this.model = config["model"] || "24h min. temp.";
+	this.serialNumberMAC = config["serialNumberMAC"] || "";
+	this.timeOffset = parseInt(config["timeOffset"]) || 0;
+	this.freq = (config["freq"] || 5) * 60000 ;
+
+	this.topic = config["topic"];
+
+	this.filename = this.topic.split("/")[1];
+
+	this.patchToRead = config["patchToRead"] || false;
+	if (this.patchToRead) {
+		try {
+			this.fs.statSync(this.patchToRead);
+		} catch (e) {
+			try {
+				this.fs.statSync("/tmp/");
+				this.patchToRead = "/tmp/";
+			} catch (e) {
+				this.patchToRead = false;
+			}
+		}
+	}
+
+	this.service = new Service.TemperatureSensor(this.name);
+	this.service.addOptionalCharacteristic(Characteristic.StatusActive);
+	this.service.addOptionalCharacteristic(Characteristic.StatusFault);
+
+	this.temperature = -40.0;
+	this.activeStat = true;
+	this.faultStat = false;
+	this.dateTime = "";
+
+	this.service
+		.getCharacteristic(Characteristic.CurrentTemperature)
+		.on('get', this.getState.bind(this));
+
+	this.service
+		.getCharacteristic(Characteristic.CurrentTemperature)
+		.setProps({
+			minValue: -50
+		});
+
+	this.service
+		.getCharacteristic(Characteristic.CurrentTemperature)
+		.setProps({
+			maxValue: 125
+		});
+
+	this.service
+		.getCharacteristic(Characteristic.StatusActive)
+		.on('get', this.getStatusActive.bind(this));
+
+	this.service
+		.getCharacteristic(Characteristic.StatusFault)
+		.on("get", this.getStatusFault.bind(this));
+
+	    this.Timestamp = function() {
+	        Characteristic.call(this, 'Timestamp', 'FF000001-0000-1000-8000-135D67EC4377');
+	        this.setProps({
+	          format: Characteristic.Formats.STRING,
+	          perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+	        });
+	        this.value = this.getDefaultValue();
+	    };
+	    inherits(this.Timestamp, Characteristic);
+		this.service.addOptionalCharacteristic(this.Timestamp);
+	
+		this.service
+			.getCharacteristic(this.Timestamp)
+			.on('get', this.getTimestamp.bind(this));
+
+	this.getState.bind(this);
+	setInterval(() => {
+		this.getState.bind(this);
+	}, this.freq);
+
+}
+
+
+MinTemperatureLogAccessory.prototype.getState = function(callback) {
+	var minMaxTmp = ["", ""];
+	try {
+		var data = this.fs.readFileSync(this.patchToRead + this.filename + "_minTemp.txt", 'utf8');
+	} catch (err) {
+		this.log(err)
+		his.activeStat = false;
+	}
+	if (!data) {
+		this.log("Problem read min. temp. file");
+		this.activeStat = false;
+	} else {
+		this.activeStat = true;
+		minMaxTmp = data.split("\t");
+		this.temperature = parseFloat(minMaxTmp[1]);
+		if((new Date(minMaxTmp[0])).getTime()>0){
+			this.dateTime = convertDateUTCDtoLocalStr(new Date(minMaxTmp[0]),this.timeOffset);
+			var date = (new Date(minMaxTmp[0])).getTime();
+			if ((new Date).getTime() - date > 90000000) {
+				this.faultStat = true;
+			} else {
+				this.faultStat = false;
+			}
+		}
+		else {
+			this.faultStat = true;
+		}
+	}
+	this.service.setCharacteristic(this.Timestamp, this.dateTime);
+	this.service.setCharacteristic(Characteristic.StatusActive, this.activeStat);
+	this.service.setCharacteristic(Characteristic.StatusFault, this.faultStat);
+
+	callback(null, this.temperature);
+}
+
+MinTemperatureLogAccessory.prototype.getStatusActive = function(callback) {
+	callback(null, this.activeStat);
+}
+
+MinTemperatureLogAccessory.prototype.getStatusFault = function(callback) {
+	callback(null, this.faultStat);
+}
+MinTemperatureLogAccessory.prototype.getTimestamp = function(callback) {
+	callback(null, this.dateTime);
+}
+
+MinTemperatureLogAccessory.prototype.getServices = function() {
+
+	var informationService = new Service.AccessoryInformation();
+
+	informationService
+		.setCharacteristic(Characteristic.Name, this.name)
+		.setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+		.setCharacteristic(Characteristic.Model, this.model)
+		.setCharacteristic(Characteristic.SerialNumber, this.serialNumberMAC);
+
+	return [informationService, this.service];
+}
